e8b34dd69ae9e45a5573ce3f09ebdec13148ffab
Radu Alexe <95101627+radualexe32@users.noreply.github.com>
2023-05-24 14:22:36 -0400

    Add plot

--- src/train.py
+++ src/train.py
@@ -7,6 +7,14 @@ from lreg import LinearRegression
 def MSE(y_test, pred):
     return np.mean((y_test - pred) ** 2)
 
+def plot(X, X_train, y_train, X_test, y_test, y_pred_line):
+    cmap = plt.get_cmap("viridis")
+    plt.figure(figsize = (8, 6))
+    plt.scatter(X_train, y_train, color = cmap(0.9), s = 10)
+    plt.scatter(X_test, y_test, color = cmap(0.5), s = 10)
+    plt.plot(X, y_pred_line, color = "black", linewidth = 1, label = "Prediction")
+    plt.show()
+
 def train():
     X, y = datasets.make_regression(n_samples = 100, n_features = 1, noise = 10, random_state = 3)
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 123)
@@ -15,14 +23,8 @@ def train():
     reg.fit(X_train, y_train)
     pred = reg.predict(X_test)
 
-    mse = MSE(y_test, pred)
-    print(f"MSE = {mse}")
-    print(f"y = {reg.w[0]:.2f}x + {reg.b:.2f}")
+    print(f"MSE = {MSE(y_test, pred)}")
+    print(f"Reg Line: y = {reg.w[0]}x + {reg.b}")
 
     y_pred_line = reg.predict(X)
-    cmap = plt.get_cmap("viridis")
-    plt.figure(figsize = (8, 6))
-    plt.scatter(X_train, y_train, color = cmap(0.9), s = 10)
-    plt.scatter(X_test, y_test, color = cmap(0.5), s = 10)
-    plt.plot(X, y_pred_line, color = "black", linewidth = 1, label = "Prediction")
-    plt.show()
+    plot(X, X_train, y_train, X_test, y_test, y_pred_line)
