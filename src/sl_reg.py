118a68127fdf2304f23579caf5bbdf6298ca9217
Radu Alexe <radualexe32@outlook.com>
2023-06-07 01:25:42 -0400

    graphs for weights, bias, mse and r2 history for both gd algorithm types

--- src/sl_reg.py
+++ src/sl_reg.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time 
 
 def MSE(y_test, pred):
     return np.mean((y_test - pred) ** 2)
@@ -11,13 +12,14 @@ class LinearRegression:
         self.rate = rate
         self.epochs = epochs
         self.w, self.b = None, None
-        self.w_hist, self.b_hist, self.mse_hist, self.r2_hist = [[] for _ in range(4)]
+        self.w_hist, self.b_hist, self.mse_hist, self.r2_hist, self.time_hist = [[] for _ in range(5)]
 
     def fit(self, X, y):
         samples, features = X.shape
         self.w = np.zeros(features)
         self.b = 0
-    
+        start = time.time()
+        
         for _ in range(self.epochs):
             y_hat = np.dot(X, self.w) + self.b
             self.w -= self.rate * (2 / samples) * np.dot(X.T, (y_hat - y))
@@ -26,11 +28,13 @@ class LinearRegression:
             self.b_hist.append(self.b)
             self.mse_hist.append(MSE(y, y_hat))
             self.r2_hist.append(R2(y, y_hat))
+            self.time_hist.append(time.time() - start)
 
     def fit_mini_batch(self, X, y, batch_size = 32):
         samples, features = X.shape
         self.w = np.zeros(features)
         self.b = 0
+        start = time.time()
     
         for _ in range(self.epochs):
             idx = np.random.permutation(samples)
@@ -48,6 +52,7 @@ class LinearRegression:
                 self.b_hist.append(self.b)
                 self.mse_hist.append(MSE(y, y_hat_all))
                 self.r2_hist.append(R2(y, y_hat_all))
+                self.time_hist.append(time.time() - start)
 
     def predict(self, X):
         return np.dot(X, self.w) + self.b
